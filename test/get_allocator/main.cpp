f99f1740274f7bbe4f1d00f863da9c8af77d6582
김재환 <woghks7859@gmail.com>
2023-01-24 12:49:34 +0900

    TEST: get_allocator 테스트 추가

---
+++ test/get_allocator/main.cpp
@@ -0,0 +1,9 @@
+#include "vector.hpp"
+
+int main() {
+  std::allocator<int>         a = std::allocator<int>();
+
+  typedef std::allocator<int> allocator_type;
+  allocator_type              p = allocator_type();
+  return 0;
+}
