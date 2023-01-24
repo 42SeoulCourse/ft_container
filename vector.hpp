68417a2da931283b8db6f334a8770d459ffd6ebf
김재환 <woghks7859@gmail.com>
2023-01-24 10:30:19 +0900

    FIX: vector.hpp 수정

--- vector.hpp
+++ vector.hpp
@@ -13,14 +13,14 @@ template <class T, class Allocator = std::allocator<T> >
 class vector {
   /********** Member types **********/
  public:
-  typedef T                            value_type;
-  typedef Allocator                    allocator_type;
-  typedef std::size_t                  size_type;
-  typedef std::ptrdiff_t               difference_type;
-  typedef value_type&                  reference;
-  typedef const value_type&            const_reference;
-  typedef Allocator::value_type*       pointer;
-  typedef Allocator::const value_type* const_pointer;
+  typedef T                                        value_type;
+  typedef Allocator                                allocator_type;
+  typedef std::size_t                              size_type;
+  typedef std::ptrdiff_t                           difference_type;
+  typedef typename allocator_type::reference       reference;
+  typedef typename allocator_type::const_reference const_reference;
+  typedef typename allocator_type::pointer         pointer;
+  typedef typename allocator_type::const_pointer   const_pointer;
   typedef typename std::iterator<std::random_access_iterator_tag, T> iterator;
   typedef typename std::iterator<std::random_access_iterator_tag, const T>
                                                 const_iterator;
@@ -28,10 +28,10 @@ class vector {
   typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
   /***************************** Member variables *****************************/
-  // private:
-  //   size_type _size;
-  //   size_type _capacity;
-  //   pointer _data;
+ private:
+  size_type _size;
+  size_type _capacity;
+  pointer   _data;
 
   /***************************** Member functions *****************************/
  public:
@@ -40,22 +40,21 @@ class vector {
   vector() : _size(0), _capacity(0), _data(NULL) {
     std::cout << "Create vector by Defalut vector constructor" << std::endl;
   };
-  explicit vector(const Allocator& alloc){
-    std::cout << "Create vector by Copy Allocator vector constructor" << std::endl;
+  explicit vector(const Allocator& alloc) {
+    std::cout << "Create vector by Copy Allocator vector constructor"
+              << std::endl;
   };
   explicit vector(size_type count, const value_type& value = value_type(),
                   const Allocator& alloc = Allocator()){};
   template <class InputIt>
-  vector(InputIt first, InputIt last, const Allocator& alloc = Allocator()){
+  vector(InputIt first, InputIt last, const Allocator& alloc = Allocator()) {
     std::cout << "Create vector by Iterator vector constructor" << std::endl;
   };
-  vector(const vector& other){
+  vector(const vector& other) {
     std::cout << "Create vector by Copy vector constructor" << std::endl;
   };
-  ~vector(){
-    std::cout << "Destructs vector" << std::endl;
-  };
-  vector& operator=(const vector& other){
+  ~vector() { std::cout << "Destructs vector" << std::endl; };
+  vector& operator=(const vector& other) {
     std::cout << "Create vector by assignment operation" << std::endl;
   };
 
