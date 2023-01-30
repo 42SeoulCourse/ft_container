4ddfc7505e7b5833fea85b7a57d89327cd74ffce
김재환 <woghks7859@gmail.com>
2023-01-30 15:10:35 +0900

    FEAT: lexicographical 함수 구현

--- utils.hpp
+++ utils.hpp
@@ -1,21 +1,33 @@
 #ifndef UTILS_HPP
-# define UTILS_HPP
+#define UTILS_HPP
 
 #include "iterator.hpp"
 
 namespace ft {
-  // distance ==================================================================
-    template <class InputIterator>
-    typename ft::iterator_traits<InputIterator>::difference_type distance(
-        InputIterator first, InputIterator last) {
-      typename ft::iterator_traits<InputIterator>::difference_type num = 0;
-      while (first != last) {
-        ++first;
-        ++num;
-      }
-      return num;
-    }
+// distance ==================================================================
+template <typename InputIterator>
+typename ft::iterator_traits<InputIterator>::difference_type distance(
+    InputIterator first, InputIterator last) {
+  typename ft::iterator_traits<InputIterator>::difference_type num = 0;
+  while (first != last) {
+    ++first;
+    ++num;
+  }
+  return num;
+}
 
-} // namespace ft
+template <typename InputIterator1, typename InputIterator2>
+bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+                             InputIterator2 first2, InputIterator2 last2) {
+  while (first1 != last1) {
+    if (first2 == last2 || *first2 < *first1) return false;
+    if (*first1 < *first2) return true;
+    ++first1;
+    ++first2;
+  }
+  return (first2 != last2);
+}
+
+}  // namespace ft
 
 #endif
