da5fe089eb56035926481ea7fd9cd2a79fb9499a
김재환 <woghks7859@gmail.com>
2023-02-08 16:19:06 +0900

    FEAT: const iterator 구현

--- red_black_tree.hpp
+++ red_black_tree.hpp
@@ -45,7 +45,90 @@ class RB_tree_iterator {
  public:
   RB_tree_iterator() : node(0) {}
   RB_tree_iterator(node_type x) : node(x) {}
-  RB_tree_iterator(const RB_tree_iterator &it) : node(it.node) {}
+  RB_tree_iterator(const RB_tree_iterator<V> &it) : node(it.node) {}
+
+  // reference operator*() const { return node_type(node)->value; }
+  reference operator*() const { return node->value; }
+  pointer   operator->() const { return &(operator*()); }
+
+  RB_tree_iterator &operator++() {
+    increment();
+    return *this;
+  }
+
+  RB_tree_iterator operator++(int) {
+    RB_tree_iterator tmp = *this;
+    increment();
+    return tmp;
+  }
+
+  RB_tree_iterator &operator--() {
+    decrement();
+    return *this;
+  }
+
+  RB_tree_iterator operator--(int) {
+    RB_tree_iterator tmp = *this;
+    decrement();
+    return tmp;
+  }
+
+  bool operator==(const RB_tree_iterator &it) const { return node == it.node; }
+  bool operator!=(const RB_tree_iterator &it) const { return node == it.node; }
+
+ protected:
+  void increment() {
+    // 오른쪽으로 갔다가, 왼쪽으로 간다.
+    if (node->right != 0) {
+      node = node->right;
+      while (node->left != 0) node = node->left;
+    } else {  // 오른쪽이 없으면, 부모로 간다. 부모가 왼쪽일 때까지 올라간다.
+              // 부모가 왼쪽이면, 부모를 리턴한다.
+      node_type y = node->parent;
+      while (node == y->right) {
+        node = y;
+        y = y->parent;
+      }
+      if (node->right != y) node = y;
+    }
+  }
+
+  void decrement() {
+    // 노드가 헤더인지 아닌지 확인. 헤더를 RED 로, 헤더의 parent 는 root 이다.
+    // head 를 end 라고 생각하면, -- 했을 때 right most 값
+    if (node->color == RED && node->parent->parent == node)
+      node = node->right;
+    else if (node->left != 0) {  // 왼쪽 자식이 있으면 왼쪽 자식의 right most 값
+      node_type y = node->left;
+      while (y->right != 0) y = y->right;
+      node = y;
+    } else {  // 왼쪽 자식이 없으면 부모로 올라간다. 부모가 오른쪽일 때까지
+              // 올라간다. 그때 오른쪽 노드의 부모
+      node_type y = node->parent;
+      while (node == y->left) {
+        node = y;
+        y = y->parent;
+      }
+      node = y;
+    }
+  }
+};
+
+template <typename V>
+class RB_tree_const_iterator {
+  typedef V                              value_type;
+  typedef const V                             &reference;
+  typedef const V                             *pointer;
+  typedef ptrdiff_t                      difference_type;
+  typedef ft::bidirectional_iterator_tag iterator_category;
+  typedef RB_tree_node<V>               *node_type;
+  node_type                              node;
+
+ public:
+  RB_tree_const_iterator() : node(0) {}
+  RB_tree_const_iterator(node_type x) : node(x) {}
+  RB_tree_const_iterator(const RB_tree_iterator<V> &it) : node(it.node) {}
+  RB_tree_const_iterator(const RB_tree_const_iterator<V> &it) : node(it.node) {}
 
   // reference operator*() const { return node_type(node)->value; }
   reference operator*() const { return node->value; }
