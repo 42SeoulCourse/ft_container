456e9cc1fd6fde3c3adfc3139254bdd12fa1710f
김재환 <woghks7859@gmail.com>
2023-02-09 16:56:12 +0900

    FEAT: pair 구현

---
+++ pair.hpp
@@ -0,0 +1,60 @@
+#ifndef PAIR_HPP
+#define PAIR_HPP
+
+namespace ft {
+
+template <typename T1, typename T2>
+struct pair {
+  T1 first;
+  T2 second;
+
+  pair() : first(), second() {}
+  pair(const T1 &t1, const T &t2) : first(t1), second(t2) {}
+
+  template <typename U1, typename U2>
+  pair(const pair<U1, U2> &p) : first(p.first), second(p.second) {}
+
+  pair &operator==(pair const &p) {
+    first = p.first;
+    second = p.second;
+    return *this;
+  }
+
+  template <typename T, typename U>
+  bool operator==(const pair<T, U> &x, const pair<T, U> &y) {
+    return x.first == y.first && x.second == y.second;
+  }
+
+  template <typename T, typename U>
+  bool operator!=(const pair<T, U> &x, const pair<T, U> &y) {
+    return !(x == y);
+  }
+
+  template <typename T, typename U>
+  bool operator<(const pair<T, U> &x, const pair<T, U> &y) {
+    return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
+  }
+
+  template <typename T, typename U>
+  bool operator>(const pair<T, U> &x, const pair<T, U> &y) {
+    return y < x;
+  }
+
+  template <typename T, typename U>
+  bool operator>=(const pair<T, U> &x, const pair<T, U> &y) {
+    return !(x < y);
+  }
+
+  template <typename T, typename U>
+  bool operator<=(const pair<T, U> &x, const pair<T, U> &y) {
+    return !(y < x);
+  }
+
+    template <typename T, typename U>
+    pair<T, U> make_pair(T x, U y) {
+      return pair<T, U>(x, y);
+    }
+};
+}  // namespace ft
+
+#endif
