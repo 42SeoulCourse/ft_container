ef0a884f6c540676467ab6672786e8e23459cc21
김재환 <woghks7859@gmail.com>
2023-02-16 17:25:56 +0900

    TEST: size & resize 테스트

---
+++ test_vector/capacity/resize_size.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+
+#include "../all.hpp"
+
+int main() {
+  // std size TEST
+  std::cout << "<--------------std size TEST-------------->" << std::endl;
+  std::cout << std::endl;
+
+  std::vector<int> stdVector;
+  std::cout << "stdVector size() : " << stdVector.size() << std::endl;
+  std::cout << "stdVector contains:";
+  for (int i = 0; i < stdVector.size(); i++)
+    std::cout << ' ' << stdVector[i];
+  std::cout << '\n';
+
+  std::cout << std::endl;
+  for (int i = 1; i < 10; i++) stdVector.push_back(i);
+  std::cout << "10 elements push_back to stdVector size() : " << stdVector.size() << std::endl;
+  std::cout << "stdVector contains:";
+  for (int i = 0; i < stdVector.size(); i++)
+    std::cout << ' ' << stdVector[i];
+  std::cout << '\n';
+
+  std::cout << std::endl;
+  stdVector.resize(5);
+  std::cout << "resize(5) stdVector size() : " << stdVector.size() << std::endl;
+  std::cout << "stdVector contains:";
+  for (int i = 0; i < stdVector.size(); i++)
+    std::cout << ' ' << stdVector[i];
+  std::cout << '\n';
+
+  std::cout << std::endl;
+  stdVector.resize(8, 100);
+  std::cout << "resize(8, 100) stdVector size() : " << stdVector.size() << std::endl;
+  std::cout << "stdVector contains:";
+  for (int i = 0; i < stdVector.size(); i++)
+    std::cout << ' ' << stdVector[i];
+  std::cout << '\n';
+
+  std::cout << std::endl;
+  stdVector.resize(12);
+  std::cout << "resize(12) stdVector size() : " << stdVector.size() << std::endl;
+  std::cout << "stdVector contains:";
+  for (int i = 0; i < stdVector.size(); i++)
+    std::cout << ' ' << stdVector[i];
+  std::cout << '\n';
+
+  // ft size TEST
+  std::cout << std::endl;
+  std::cout << "<--------------ft size TEST-------------->" << std::endl;
+  std::cout << std::endl;
+
+  ft::vector<int> ftVector;
+  std::cout << "ftVector size() : " << ftVector.size() << std::endl;
+  std::cout << "stdVector contains:";
+  for (int i = 0; i < ftVector.size(); i++)
+    std::cout << ' ' << ftVector[i];
+  std::cout << '\n';
+
+  std::cout << std::endl;
+  for (int i = 1; i < 10; i++) ftVector.push_back(i);
+  std::cout << "10 elements push_back to ftVector size() : " << ftVector.size() << std::endl;
+  std::cout << "ftVector contains:";
+  for (int i = 0; i < ftVector.size(); i++)
+    std::cout << ' ' << ftVector[i];
+  std::cout << '\n';
+
+  std::cout << std::endl;
+  ftVector.resize(5);
+  std::cout << "resize(5) ftVector size() : " << ftVector.size() << std::endl;
+  std::cout << "ftVector contains:";
+  for (int i = 0; i < ftVector.size(); i++)
+    std::cout << ' ' << ftVector[i];
+  std::cout << '\n';
+
+  std::cout << std::endl;
+  ftVector.resize(8, 100);
+  std::cout << "resize(8, 100) ftVector size() : " << ftVector.size() << std::endl;
+  std::cout << "ftVector contains:";
+  for (int i = 0; i < ftVector.size(); i++)
+    std::cout << ' ' << ftVector[i];
+  std::cout << '\n';
+
+  std::cout << std::endl;
+  ftVector.resize(12);
+  std::cout << "resize(12) ftVector size() : " << ftVector.size() << std::endl;
+  std::cout << "ftVector contains:";
+  for (int i = 0; i < ftVector.size(); i++)
+    std::cout << ' ' << ftVector[i];
+  std::cout << '\n';
+
+  return 0;
+}
